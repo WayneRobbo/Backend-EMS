1df4b8d568b62b7023be6a51742d61776bb3ca01
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-20 20:11:59 +0100

    Adding-ShiftCompent-getShiftById to get shift 16

--- src/main/java/com/employeeManagementSystem/controller/ShiftController.java
+++ src/main/java/com/employeeManagementSystem/controller/ShiftController.java
@@ -1,8 +1,10 @@
 package com.employeeManagementSystem.controller;
 
+import com.employeeManagementSystem.exception.ResourceNotFoundException;
 import com.employeeManagementSystem.model.Shift;
 import com.employeeManagementSystem.repository.ShiftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +28,13 @@ public class ShiftController {
     @PostMapping("/shifts")
     public Shift createShift(@RequestBody Shift shift){
         return shiftRepository.save(shift);
+    }
+
+    //get shift by id
+    @GetMapping("/shifts/{id}")
+    public ResponseEntity<Shift> getShiftById(@PathVariable Long id){
+        Shift shift = shiftRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Shift not found with id:" + id));
+        return ResponseEntity.ok(shift);
     }
 }
