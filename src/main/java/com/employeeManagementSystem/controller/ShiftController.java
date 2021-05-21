4e31977341182b7e2390fd95da86b76575831a2e
Wayne Robinson <l00162024@student.lyit.ie>
2021-05-21 09:05:04 +0100

    Adding-DeleteMethod-ShiftById to get shift 21

--- src/main/java/com/employeeManagementSystem/controller/ShiftController.java
+++ src/main/java/com/employeeManagementSystem/controller/ShiftController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -52,5 +54,17 @@ public class ShiftController {
 
         Shift updatatedShift= shiftRepository.save(shift);
         return ResponseEntity.ok(updatatedShift);
+    }
+
+    //Delete shift by rest api
+    @DeleteMapping("/shifts/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteShift(@PathVariable Long id){
+        Shift shift= shiftRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Shift not found with id:" + id));
+
+        shiftRepository.delete(shift);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
